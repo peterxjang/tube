80a0bfaec08969c7782ad4ffaa60a552434879b9
Peter Jang <peter.x.jang@gmail.com>
2024-08-22 07:10:25 -0500

    Add caching to image loading for improved performance

---
+++ tvOSTube/ThumbnailView.swift
@@ -0,0 +1,38 @@
+import InvidiousKit
+import SwiftUI
+
+struct ThumbnailView: View {
+    let width: CGFloat
+    let height: CGFloat
+    let radius: CGFloat
+    var thumbnails: [ThumbnailObject]
+
+    var preferredThumbnail: ThumbnailObject? {
+        thumbnails
+            .sorted { $0.width <= $1.width }
+            .first { $0.width >= Int(width) }
+    }
+
+    var body: some View {
+        Rectangle().foregroundStyle(.background)
+            .frame(maxWidth: width, maxHeight: height)
+            .aspectRatio(16 / 9, contentMode: .fill)
+            .background(Rectangle().foregroundStyle(.background))
+            .overlay {
+                if let thumbnailUrl = preferredThumbnail?.url, let url = URL(string: thumbnailUrl) {
+                    CacheAsyncImage(url: url) { image in
+                        image.resizable().scaledToFill()
+                            .frame(maxWidth: width, maxHeight: height)
+                    } placeholder: {}
+                        .frame(maxWidth: width, maxHeight: height)
+                        .clipped()
+                }
+            }
+            .cornerRadius(radius)
+            .clipped()
+    }
+}
+
+#Preview {
+    ThumbnailView(width: 100, height: 56.25, radius: 4.0, thumbnails: [])
+}
