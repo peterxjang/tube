f49c61d14ae6d934b6f82ca8c1584217fc43e3bf
Peter Jang <peter.x.jang@gmail.com>
2024-08-27 14:18:32 -0500

    Refactor to use consistent navigation approach

--- tvOSTube/SubscriptionsView.swift
+++ tvOSTube/SubscriptionsView.swift
@@ -1,39 +1,39 @@
 import InvidiousKit
-import Observation
 import SwiftData
 import SwiftUI
 
 struct SubscriptionsView: View {
     @Query(sort: \FollowedChannel.name) var channels: [FollowedChannel]
-    @State private var selectedChannel: FollowedChannel? = nil
     @Binding var resetView: Bool
+    @EnvironmentObject var navigationManager: NavigationManager
 
     var body: some View {
-        NavigationView {
-            ScrollView {
-                LazyVStack(alignment: .leading) {
-                    if channels.isEmpty {
-                        MessageBlock(
-                            title: "You aren't following any channels.",
-                            message: "Search for channels you'd like to follow."
-                        ).padding(.horizontal)
-                    } else {
-                        VStack(alignment: .leading) {
-                            ForEach(channels) { channel in
-                                NavigationLink(destination: ChannelView(model: ChannelViewModel(channelId: channel.id))) {
-                                    HStack {
-                                        Text(channel.name)
-                                            .padding()
-                                        Spacer()
-                                        Image(systemName: "chevron.right")
-                                            .padding(.trailing)
-                                    }
+        ScrollView {
+            LazyVStack(alignment: .leading) {
+                if channels.isEmpty {
+                    MessageBlock(
+                        title: "You aren't following any channels.",
+                        message: "Search for channels you'd like to follow."
+                    ).padding(.horizontal)
+                } else {
+                    VStack(alignment: .leading) {
+                        ForEach(channels) { channel in
+                            Button(action: {
+                                navigationManager.navigateToChannel(with: channel.id)
+                            }) {
+                                HStack {
+                                    Text(channel.name)
+                                        .padding()
+                                    Spacer()
+                                    Image(systemName: "chevron.right")
+                                        .padding(.trailing)
                                 }
                             }
                         }
                     }
-                }.padding(50)
+                }
             }
+            .padding(50)
         }
         .id(resetView) // Reset the view by changing the id whenever resetView changes
     }
