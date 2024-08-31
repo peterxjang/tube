9319b3ff127fe964a164d84a6a11f0cdf048f4d3
Peter Jang <peter.x.jang@gmail.com>
2024-08-30 22:53:22 -0500

    Change from grid to horizontal scroll to make room

--- tvOSTube/SavedVideosView.swift
+++ tvOSTube/SavedVideosView.swift
@@ -7,31 +7,35 @@ struct SavedVideosView: View {
     @Query private var savedVideos: [SavedVideo]
 
     var body: some View {
-        VStack {
-            if savedVideos.isEmpty {
-                Text("No videos saved to Watch Later.")
-                    .foregroundStyle(.secondary)
-                    .font(.headline)
-                    .padding()
-            } else {
-                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())], spacing: 50) {
-                    ForEach(savedVideos, id: \.id) { video in
-                        VideoCard(
-                            id: video.id,
-                            title: video.title,
-                            duration: video.duration,
-                            published: video.published,
-                            thumbnails: [ThumbnailObject(quality: video.quality, url: video.url, width: video.width, height: video.height)],
-                            author: video.author,
-                            authorId: "UNAVAILABLE",
-                            viewCountText: video.viewCountText
-                        )
-                        .padding(30)
+        ScrollView {
+            VStack(alignment: .leading) {
+                if savedVideos.isEmpty {
+                    Text("No videos saved to Watch Later.")
+                        .foregroundStyle(.secondary)
+                        .font(.headline)
+                        .padding()
+                } else {
+                    Text("Watch Later")
+                        .font(.largeTitle)
+                    ScrollView(.horizontal) {
+                        LazyHGrid(rows: [.init(.flexible(minimum: 600))], alignment: .top, spacing: 70.0) {
+                            ForEach(savedVideos) { video in
+                                VideoCard(
+                                    id: video.id,
+                                    title: video.title,
+                                    duration: video.duration,
+                                    published: video.published,
+                                    thumbnails: [ThumbnailObject(quality: video.quality, url: video.url, width: video.width, height: video.height)],
+                                    author: video.author,
+                                    authorId: "UNAVAILABLE",
+                                    viewCountText: video.viewCountText
+                                )
+                            }
+                        }.padding(20)
                     }
                 }
-                .padding(50)
             }
+            .navigationTitle("Watch Later")
         }
-        .navigationTitle("Watch Later")
     }
 }
