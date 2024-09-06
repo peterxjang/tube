b4bf9dd074f31bddf873c8f721b01da567248fa1
Peter Jang <peter.x.jang@gmail.com>
2024-09-05 21:02:26 -0500

    Add sponsorblock segments

--- tvOSTube/OpenVideoPlayerAction.swift
+++ tvOSTube/OpenVideoPlayerAction.swift
@@ -7,6 +7,24 @@ import MediaPlayer
 import Observation
 import Combine
 
+public struct SponsorBlockObject: Decodable {
+    public var category: String
+    public var actionType: String
+    public var segment: [Float]
+    public var UUID: String
+    public var videoDuration: Float
+    public var locked: Int
+    public var votes: Int
+    public var description: String
+}
+
+struct Chapter {
+    let title: String
+    let imageName: String
+    let startTime: TimeInterval
+    let endTime: TimeInterval
+}
+
 enum VideoPlaybackError: LocalizedError {
     case missingUrl
 }
@@ -20,6 +38,7 @@ final class OpenVideoPlayerAction {
     private var statusObserver: AnyCancellable?
     private var timeObserverToken: Any? = nil
     var watchedSeconds: Double = 0.0
+    private var skippableSegments: [[Float]] = []
 
     var currentPlayer: AVPlayer? {
         return player
@@ -93,20 +112,78 @@ final class OpenVideoPlayerAction {
         }
 
         var metadata: [AVMetadataItem] = []
-        let titleMetadata = AVMutableMetadataItem()
-        titleMetadata.identifier = .commonIdentifierTitle
-        titleMetadata.value = video.title as any NSCopying & NSObjectProtocol
-        titleMetadata.extendedLanguageTag = "und"
-        metadata.append(titleMetadata)
-
-        let subtitleMetadata = AVMutableMetadataItem()
-        subtitleMetadata.identifier = .iTunesMetadataTrackSubTitle
-        subtitleMetadata.value = video.author as any NSCopying & NSObjectProtocol
-        subtitleMetadata.extendedLanguageTag = "und"
-        metadata.append(subtitleMetadata)
-
+        metadata.append(makeMetadataItem(.commonIdentifierTitle, value: video.title))
+        metadata.append(makeMetadataItem(.iTunesMetadataTrackSubTitle, value: video.author))
         item.externalMetadata = metadata
+
+        getSponsorSegments(video: video, playerItem: item)
+        
         return item
+    }
+    
+    private func getSponsorSegments(video: Video, playerItem: AVPlayerItem) {
+        let url = URL(string: "https://sponsor.ajay.app/api/skipSegments?videoID=\(video.videoId)")!
+        let request = URLRequest(url: url)
+        Task {
+            do {
+                let (data, _) = try await URLSession.shared.data(for: request)
+                if let videoInfo = try? JSONDecoder().decode([SponsorBlockObject].self, from: data) {
+                    print(videoInfo)
+                    skippableSegments = videoInfo.map { $0.segment }
+                    playerItem.navigationMarkerGroups = makeNavigationMarkerGroups(video: video)
+                } else {
+                    print("Invalid Response")
+                }
+            } catch {
+                print("Failed to Send POST Request \(error)")
+            }
+        }
+    }
+
+    private func makeNavigationMarkerGroups(video: Video) -> [AVNavigationMarkersGroup] {
+        var chapters: [Chapter] = []
+        var previousEndTime: TimeInterval = 0.0
+        for (index, segment) in skippableSegments.enumerated() {
+            let startTime = TimeInterval(segment[0])
+            let endTime = TimeInterval(segment[1])
+            if previousEndTime < startTime {
+                chapters.append(Chapter(title: "Chapter \(chapters.count + 1)", imageName: "chapter\(chapters.count + 1)", startTime: previousEndTime, endTime: startTime))
+            }
+            chapters.append(Chapter(title: "Advertisement \(index + 1)", imageName: "ad", startTime: startTime, endTime: endTime))
+            previousEndTime = endTime
+        }
+        if previousEndTime < TimeInterval(video.lengthSeconds) {
+            chapters.append(Chapter(title: "Chapter \(chapters.count + 1)", imageName: "chapter\(chapters.count + 1)", startTime: previousEndTime, endTime: TimeInterval(video.lengthSeconds)))
+        }
+        var metadataGroups = [AVTimedMetadataGroup]()
+        chapters.forEach { chapter in
+            metadataGroups.append(makeTimedMetadataGroup(for: chapter))
+        }
+        return [AVNavigationMarkersGroup(title: nil, timedNavigationMarkers: metadataGroups)]
+    }
+
+    private func makeTimedMetadataGroup(for chapter: Chapter) -> AVTimedMetadataGroup {
+        var metadata = [AVMetadataItem]()
+        let titleItem = makeMetadataItem(.commonIdentifierTitle, value: chapter.title)
+        metadata.append(titleItem)
+        if let image = UIImage(named: chapter.imageName),
+           let pngData = image.pngData() {
+            let imageItem = makeMetadataItem(.commonIdentifierArtwork, value: pngData)
+            metadata.append(imageItem)
+        }
+        let timescale: Int32 = 600
+        let startTime = CMTime(seconds: chapter.startTime, preferredTimescale: timescale)
+        let endTime = CMTime(seconds: chapter.endTime, preferredTimescale: timescale)
+        let timeRange = CMTimeRangeFromTimeToTime(start: startTime, end: endTime)
+        return AVTimedMetadataGroup(items: metadata, timeRange: timeRange)
+    }
+
+    private func makeMetadataItem(_ identifier: AVMetadataIdentifier, value: Any) -> AVMetadataItem {
+        let item = AVMutableMetadataItem()
+        item.identifier = identifier
+        item.value = value as? NSCopying & NSObjectProtocol
+        item.extendedLanguageTag = "und"
+        return item.copy() as! AVMetadataItem
     }
 
     private func updateNowPlayingInfo(with video: Video) {
@@ -124,7 +201,18 @@ final class OpenVideoPlayerAction {
         }
         let interval = CMTime(seconds: 1, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
         timeObserverToken = player.addPeriodicTimeObserver(forInterval: interval, queue: .main) { [weak self] time in
-            self?.watchedSeconds = CMTimeGetSeconds(time)
+            guard let self = self else { return }
+            let currentTime = CMTimeGetSeconds(time)
+            for segment in self.skippableSegments {
+                let startTime = segment[0]
+                let endTime = segment[1]
+                if currentTime >= Double(startTime) && currentTime < Double(endTime) {
+                    print("Current \(currentTime), Skipping segment: \(startTime) to \(endTime)")
+                    self.player?.seek(to: CMTime(seconds: Double(endTime + 1.0), preferredTimescale: 1), toleranceBefore: CMTime.zero, toleranceAfter: CMTime.positiveInfinity)
+                    break
+                }
+            }
+            self.watchedSeconds = currentTime
         }
     }
 }
